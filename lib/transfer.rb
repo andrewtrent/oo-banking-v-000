a074e0a58c8783166ceec712faffe064cb3bc258
Andrew Trent <andrewtrentdesign@gmail.com>
2020-08-26 15:39:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    true if self.sender.valid? && self.receiver.valid?
+    true if self.sender.valid? && self.receiver.valid? else false
   end
 
   def execute_transaction
